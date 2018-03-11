8d7125757379570832d330991a4894f1452c2300
Gareth George <garethgeorge97@gmail.com>
2018-03-10 17:46:42 -0800

    database and file system state are now persisted properly in volumes

--- submit/runtime_scripts/start_application.sh
+++ submit/runtime_scripts/start_application.sh
@@ -8,11 +8,11 @@ else
 fi
 
 # # Prepare the application's database
-if [ ! -S /home/submit/files/dbready.txt ]; then
+if [ ! -f /home/submit/submit-files/dbready.txt ]; then
   echo "Getting the application's database ready"
   su submit -c 'source /home/submit/venv/bin/activate; echo "from submit import models; models.create_schema()" | pshell /home/submit/files/submit.ini'
   su submit -c 'source /home/submit/venv/bin/activate; cat /tmp-submit/create_admin_user.py | pshell /home/submit/files/submit.ini'
-  touch /home/submit/files/dbready.txt 
+  touch /home/submit/submit-files/dbready.txt 
 else
-  echo "Found /home/submit/files/dbready.txt indicating the database is already set up, skipping database ready as it may be redundant and dangerous. If you have made changes to the database please clear out the file system and the database"
+  echo "Found /home/submit/submit-files/dbready.txt indicating the database is already set up, skipping database ready as it may be redundant and dangerous. If you have made changes to the database please clear out the file system and the database"
 fi
